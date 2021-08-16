c40e21318e4545e92dc550bfbf3bdf7a5631e140
JakeSynth <jgheath54@gmail.com>
2021-08-16 15:31:10 +0100

    Update L.Control.Sidebar.js

--- scripts/L.Control.Sidebar.js
+++ scripts/L.Control.Sidebar.js
@@ -32,7 +32,7 @@ L.Control.Sidebar = L.Control.extend({
         if (this.options.closeButton) {
             var close = this._closeButton =
                 L.DomUtil.create('a', 'close', container);
-            close.innerHTML = '&rarrhk;';
+            close.innerHTML = '&rarrhk;&#xFE0E;';
         }
     },
 
